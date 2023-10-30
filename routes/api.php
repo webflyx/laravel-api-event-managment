ae61198d33c1b82c502d51f77400192b6d7b7a8c
webflyx <zavala180395@gmail.com>
2023-10-30 10:12:42 +0200

    Added AuthController

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\Api\AttendeeController;
 use App\Http\Controllers\Api\EventController;
+use App\Http\Controllers\AuthController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -19,6 +20,8 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::post('login', [AuthController::class,'login']);
 
 Route::apiResource('events', EventController::class);
 
