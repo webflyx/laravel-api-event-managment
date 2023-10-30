46b7cd020adff8841843631b6b1dac86c4ed16f1
webflyx <zavala180395@gmail.com>
2023-10-30 17:04:25 +0200

    Added DB queue for notification

--- app/Notifications/EventReminderNotification.php
+++ app/Notifications/EventReminderNotification.php
@@ -9,7 +9,7 @@ use Illuminate\Notifications\Notification;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Notifications\Messages\MailMessage;
 
-class EventReminderNotification extends Notification
+class EventReminderNotification extends Notification implements ShouldQueue
 {
     use Queueable;
 
